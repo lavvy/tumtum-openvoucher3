6b68a4c2ba083e28fbaa8b5db13b26b4787e7236
lavvy <lavashonline@gmail.com>
2016-02-22 15:00:27 -0800

    still doin

--- create_mysql_admin_user.sh
+++ create_mysql_admin_user.sh
@@ -19,6 +19,12 @@ mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
 
 ###################################################################################################################
 #my own little hark
+################# my little hack ##################
+wget -O /tmp/package.tar.gz https://github.com/litzinetz-de/OpenVoucher/archive/0.4.2.tar.gz
+tar -zxf /tmp/package.tar.gz -C /tmp/
+cp -pr /tmp/OpenVoucher-*/src/* /app/
+rm -rf /app/.htaccess
+################################################################
 mysql -uroot -e "CREATE USER 'local'@'%' IDENTIFIED BY 'local'"     
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'local'@'%' WITH GRANT OPTION"                                                                            
 #mysql -uopenvoucher -popenvoucher </app2/database/tables.sql 
