f428a282959a0e88a58211b447a607b8d1ae444f
lavvy <lavashonline@gmail.com>
2016-05-13 22:33:02 -0700

    Update create_mysql_admin_user.sh

--- create_mysql_admin_user.sh
+++ create_mysql_admin_user.sh
@@ -20,7 +20,7 @@ mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
 ###################################################################################################################
 #my own little hark
 ################# my little hack ##################
-wget -O /tmp/package.tar.gz https://github.com/litzinetz-de/OpenVoucher/archive/0.4.2.tar.gz
+wget -O /tmp/package.tar.gz https://github.com/litzinetz-de/OpenVoucher/archive/0.4.1.tar.gz
 tar -zxf /tmp/package.tar.gz -C /tmp/
 cp -pr /tmp/OpenVoucher-*/src/* /app/
 rm -rf /app/.htaccess
