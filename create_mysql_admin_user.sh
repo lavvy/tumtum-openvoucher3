1c0004c68a2e3958252e41b40cf5dad786790ec4
lavvy <lavashonline@gmail.com>
2016-05-31 09:38:51 -0700

    add back html folder

--- create_mysql_admin_user.sh
+++ create_mysql_admin_user.sh
@@ -22,8 +22,8 @@ mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
 ################# my little hack ##################
 wget -O /tmp/package.tar.gz https://github.com/litzinetz-de/OpenVoucher/archive/0.4.2.tar.gz
 tar -zxf /tmp/package.tar.gz -C /tmp/
-cp -pr /tmp/OpenVoucher-*/src/* /var/www
-rm -rf /var/www/.htaccess
+cp -pr /tmp/OpenVoucher-*/src/* /var/www/html
+#rm -rf /var/www/.htaccess
 ################################################################
 mysql -uroot -e "CREATE USER 'local'@'%' IDENTIFIED BY 'local'"     
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'local'@'%' WITH GRANT OPTION"                                                                            
