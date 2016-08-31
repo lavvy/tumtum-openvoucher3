7da587f01601f594d4c65d6ff896debb9f823844
lavvy <lavashonline@gmail.com>
2016-08-31 22:11:45 +0000

    Update 'webhook/index.php'

--- webhook/index.php
+++ webhook/index.php
@@ -1,15 +1,17 @@
+
 <?php
 
 $data = json_decode(file_get_contents('php://input'), true);
  
 $src = $data["repository"]["clone_url"];
 
-$name = "/var/www/html/".$data["repository"]["name"];
+$reponame = $data["repository"]["name"];
 
-$cmd = "git clone ".$src." && cd ".$name." && chmod 777 -R ".$name." && ./run.sh";
+$cmd ="export REPONAME=".$reponame." && export SRC=".$src." && /webhook.sh";
 
 $out = shell_exec($cmd);
 
 echo $cmd;
 
 ?>
+
